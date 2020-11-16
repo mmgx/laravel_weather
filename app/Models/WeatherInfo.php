446415960f1639f710ed884d4cd427f59d4bdf75
Mihail M <r@gothundead.ru>
2020-11-16 16:25:30 +0300

    Добавлены связи в модели и обновлены свойства $casts

--- app/Models/WeatherInfo.php
+++ app/Models/WeatherInfo.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use App\Models\Base\Model as BaseModel;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class WeatherInfo extends BaseModel
 {
     use HasFactory;
+
+    protected $casts = [
+        'temperature_c' => 'double',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
 
     /**
      * Статусы обновления погоды у городов
@@ -16,4 +23,8 @@ class WeatherInfo extends BaseModel
     public const STATUS_INACTIVE = 'INACTIVE';
     public static $statuses = [self::STATUS_ACTIVE, self::STATUS_INACTIVE];
 
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class);
+    }
 }
