4fd12c235fbb9aa84bcb607be759c7bd477e1779
Mihail M <r@gothundead.ru>
2020-11-16 14:23:30 +0300

    Добавлена миграция и модель WeatherInfo

---
+++ app/Models/WeatherInfo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Base\Model as BaseModel;
+
+class WeatherInfo extends BaseModel
+{
+    use HasFactory;
+}
