bbd6f70df23094e3e9dab19fcf39f5021c8112b9
Mihail M <r@gothundead.ru>
2020-11-16 16:00:19 +0300

    обновлен $casts в модели User

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,5 +39,7 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
     ];
 }
