f55623eb8746645b52ac25ce88a18dbc04d97690
Mihail M <r@gothundead.ru>
2020-11-16 20:59:11 +0300

    Обновлены City и BaseController

---
+++ app/Http/Controllers/API/CityController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\API\Base\BaseController;
+use App\Models\City;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
+
+class CityController extends BaseController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return JsonResponse
+     */
+    public function index(): JsonResponse
+    {
+        $city = City::all();
+        return $this->sendResponse($city->toArray(), 'Список городов получен успешно');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param City $city
+     * @return JsonResponse
+     */
+    public function show(City $city)
+    {
+        $product = City::find($city);
+        if (is_null($city)) {
+            return $this->sendError('Product not found.');
+        }
+        return $this->sendResponse($product->toArray(), 'Город успешно получен');
+    }
+}
