95721d6091205d24a58b20139fb9ef03b50a74d8
Mihail M <r@gothundead.ru>
2020-11-16 19:30:41 +0300

    обновлены Сервисы и UserRepository

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -2,7 +2,27 @@
 
 namespace App\Repository;
 
+use App\Models\User as Model;
+
 class UserRepository extends Base\BaseRepository
 {
+    /**
+     * Получить название класса
+     * @return string
+     */
+    protected function getModelClass(): string
+    {
+        return Model::class;
+    }
 
+    public function __construct(Model $user)
+    {
+        $this->model = $user;
+    }
+
+
+    public function model(): string
+    {
+        return $this->getModelClass();
+    }
 }
--- app/Service/UserService.php
+++ app/Service/UserService.php
@@ -2,7 +2,16 @@
 
 namespace App\Service;
 
+use App\Models\User as Model;
+
 class UserService extends Base\BaseService
 {
-
+    /**
+     * UserService constructor.
+     * @param  Model  $user
+     */
+    public function __construct(Model $user)
+    {
+        $this->model = $user;
+    }
 }
