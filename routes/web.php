91eb3b4508ac7ba4a1ade5ececa11f0886d2bc52
Mihail M <r@gothundead.ru>
2020-11-17 11:49:55 +0300

    добавлены маршуты UserController

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,8 @@
 <?php
 
+use App\Http\Controllers\Backend\User\UserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -20,3 +11,34 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+
+Route::group([
+    'prefix' => 'admin',
+    'as' => 'admin.auth.user.',
+], function () {
+
+    /**
+     * Пользователи
+     */
+    Route::group([
+        'prefix' => 'users',
+    ], function () {
+        Route::get('/', [UserController::class, 'index'])->name('index');
+        Route::delete('/', [UserController::class, 'destroy'])->name('destroy');
+        Route::post('/', [UserController::class, 'store'])->name('store');
+        Route::get('create', [UserController::class, 'create'])->name('create');
+    });
+
+    Route::group(['prefix' => 'users/{user}'], function () {
+        Route::get('show', [UserController::class, 'show'])->name('show');
+        Route::get('edit', [UserController::class, 'edit'])->name('edit');
+        Route::patch('/', [UserController::class, 'update'])->name('update');
+        Route::delete('/delete', [UserController::class, 'delete'])->name('delete');
+    });
+
+    Route::group(['prefix' => 'users/{deletedUser}'], function () {
+        Route::patch('restore', [UserController::class, 'restore'])->name('restore');
+        Route::delete('destroy', [UserController::class, 'destroy'])->name('destroy');
+    });
+});
