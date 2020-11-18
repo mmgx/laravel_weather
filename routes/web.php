74cbda8d9bb14adece7a2b97995e164eec047f45
Mihail M <r@gothundead.ru>
2020-11-18 14:58:22 +0300

    изменены файлы маршрутов

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Backend\Geo\CityController;
 use App\Http\Controllers\Backend\User\UserController;
+use App\Http\Controllers\QueryController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -8,7 +10,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+Route::middleware(['auth:sanctum'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
@@ -16,10 +18,11 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 Route::group([
     'prefix' => 'admin',
     'as' => 'admin.auth.user.',
+    'middleware' => 'auth:sanctum',
 ], function () {
 
     /**
-     * Пользователи
+     * Управление пользователями
      */
     Route::group([
         'prefix' => 'users',
@@ -40,5 +43,42 @@ Route::group([
     Route::group(['prefix' => 'users/{deletedUser}'], function () {
         Route::patch('restore', [UserController::class, 'restore'])->name('restore');
         Route::delete('destroy', [UserController::class, 'destroy'])->name('destroy');
+    });
+});
+
+/**
+ * Просмотр информации о погоде в городах
+ */
+
+Route::group([
+    'prefix' => 'admin',
+    'as' => 'admin.',
+    'middleware' => 'auth:sanctum',
+], function () {
+
+    Route::group([
+        'prefix' => 'cities',
+        'as' => 'geo.cities.',
+    ], function () {
+
+        Route::get('/', [CityController::class, 'index'])->name('index');
+
+        Route::group(['prefix' => '{city}'], function () {
+            Route::get('show', [CityController::class, 'show'])->name('show');
+            Route::get('history', [CityController::class, 'history'])->name('history');
+        });
+    });
+});
+
+/**
+ * Тестовые маршруты получения погоды без проверки токена
+ */
+Route::group([
+], function () {
+    Route::group([
+        'prefix' => 'test/weather',
+    ], function () {
+        Route::get('city/{city}/current', [QueryController::class ,'current']);
+        Route::get('city/{city}/all', [QueryController::class ,'all']);
     });
 });
