67eecd98ecde8b203b63b5a94c2a47f7500735fc
Mihail M <r@gothundead.ru>
2020-11-18 11:48:14 +0300

    добавлены функции getJWTIdentifier и getJWTCustomClaims в модель User

--- app/Traits/Method/UserMethod.php
+++ app/Traits/Method/UserMethod.php
@@ -11,4 +11,24 @@ trait UserMethod
     {
         return $this->id === 1;
     }
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
